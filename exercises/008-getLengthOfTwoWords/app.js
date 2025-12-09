8addbd196aa45892c13671b053f4b4b14d07d247
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2025-12-09 22:20:16 +0000

    Updates to exercises 5 - 9

--- exercises/008-getLengthOfTwoWords/app.js
+++ exercises/008-getLengthOfTwoWords/app.js
@@ -1,4 +1,8 @@
 function getLengthOfTwoWords(word1, word2) {
   // your code here
-  
+  let num1 = word1.length;
+  let num2 = word2.length;
+  return num1 + num2;
 }
+
+console.log(getLengthOfTwoWords("hi","hi"));
