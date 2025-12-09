4d05ac4e478eca9378962206ff05ffc604e929dc
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2025-12-09 23:27:41 +0000

    Worked on exercises 10 - 12

--- exercises/010-isSameLength/app.js
+++ exercises/010-isSameLength/app.js
@@ -1,1 +1,12 @@
-// Write your function here
+function isSameLength(w1, w2){
+    let word1 = w1.length;
+    let word2 = w2.length;
+    if(word1 == word2){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(isSameLength("Hello", "Hi"));
--- exercises/011-isGreaterThanTen/app.js
+++ exercises/011-isGreaterThanTen/app.js
@@ -1,4 +1,10 @@
-function isGreaterThanTen(num) {
-  // your code here
-  
+function isGreaterThanTen(num){
+  if(num > 10){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
+
+console.log(isGreaterThanTen("2"))
